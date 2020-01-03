203363f2423276cddd73912408f1439af0f2c466
Adil <adil-75@live.fr>
2020-01-04 00:24:44 +0100

    Init

---
+++ Assets/Scripts/CameraFunctions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFunctions : MonoBehaviour
+{
+    public int nbScreens = 3;
+    public int currentScreen = 0;
+    public Vector3 cameraPosition;
+    public Camera myCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCamera = Camera.main;
+        cameraPosition = myCamera.transform.position;
+    }
+
+    public void moveCameraToRight() {
+        currentScreen = (currentScreen + 1) % 3;
+        Vector3 newPosition = new Vector3(myCamera.transform.position.x, myCamera.transform.position.y, currentScreen*15);
+        myCamera.transform.position = newPosition;
+    }
+
+    public void moveCameraToLeft() {
+        currentScreen -= 1;
+        if (currentScreen < 0)
+            currentScreen = nbScreens - 1;
+        Vector3 newPosition = new Vector3(myCamera.transform.position.x, myCamera.transform.position.y, currentScreen * 15);
+        myCamera.transform.position = newPosition;
+    }
+}
---
+++ Assets/Scripts/ShapeComponent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeComponent : MonoBehaviour
+{
+    public enum Shape { Sphere, Cube, Prism, Oval, Cylinder};
+    public enum Color { Red, Green, Blue};
+    public Shape myShape;
+    public Color myColor;
+    public Texture UIImage;
+}
